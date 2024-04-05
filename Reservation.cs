41a99e1cb347acd67de493be705a5b03d07060f7
ALARAAKANTURK <c1812031@cankaya.edu.tr>
2024-04-05 15:26:17 +0300

    reservation.cs edited

--- Reservation.cs
+++ Reservation.cs
@@ -1,24 +1,6 @@
 using System;
-using System.Text.Json.Serialization;
-public record Reservation
-{ 
-    
-    [JsonPropertyName("reserverName")]
-     public string? ReserverName { get; set; }
-     
-    [JsonPropertyName("room")]
-    public Room? Room { get; set; }
-    
-    [JsonPropertyName("date")]
-    public DateTime Date{ get; set; }
-    
-    [JsonPropertyName("time")]
-    public TimeSpan Time { get; set; }
 
-    public Reservation()
-    {
-        // Initialize non-nullable properties with default values
-        Date = DateTime.MinValue;
-        Time = TimeSpan.Zero;
-    }
-}
+public record Reservation(string ReserverName, Room Room, DateTime Date, DateTime Time);
+
+
+    
