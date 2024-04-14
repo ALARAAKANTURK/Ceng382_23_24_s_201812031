69776b353082ab3fa805c028437bd2e12b65666a
ALARAAKANTURK <c1812031@cankaya.edu.tr>
2024-04-14 21:41:55 +0300

    IReservationService.cs is created and its edited acording to class diagram

--- IReservationService.cs
+++ IReservationService.cs
@@ -1,1 +1,10 @@
 
+namespace ReservationApp
+{
+    public interface IReservationService
+    {
+        public void AddReservation(Reservation reservation, string reserverName);
+        public void DeleteReservation(Reservation reservation);
+        public void DisplayWeeklySchedule();
+    }
+}
