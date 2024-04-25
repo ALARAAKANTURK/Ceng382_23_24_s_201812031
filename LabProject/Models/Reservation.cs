88f25e93e7b5e07f3691456b9260a3c748aa57ea
ALARAAKANTURK <c1812031@cankaya.edu.tr>
2024-04-25 11:56:20 +0300

    Reservation migration is created

--- LabProject/Models/Reservation.cs
+++ LabProject/Models/Reservation.cs
@@ -2,7 +2,7 @@ public class Reservation
 {       
     public int Id { get; set; }
     public string ReserverName { get; set; }
-    public int RoomId { get; set; }
+    public int RoomId { get; set; } //Foreign key
     public Room Room { get; set; }
     public DateTime ReservationDate { get; set; }
 
