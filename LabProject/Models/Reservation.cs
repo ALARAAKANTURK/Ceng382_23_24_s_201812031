214568cee46f4bbaa60a097f117cc17377b6a109
ALARAAKANTURK <c1812031@cankaya.edu.tr>
2024-04-25 11:45:05 +0300

    Reservation.cs objects are created

---
+++ LabProject/Models/Reservation.cs
@@ -0,0 +1,13 @@
+public class Reservation
+{       
+    public int Id { get; set; }
+    public string ReserverName { get; set; }
+    public Room Room { get; set; }
+    public DateTime ReservationDate { get; set; }
+
+
+      
+
+
+
+}
