19d69add3ffd8d5654782addbf86174ab6a7e422
ALARAAKANTURK <c1812031@cankaya.edu.tr>
2024-04-25 12:01:42 +0300

    UMLs folder is created

--- LabProject/Models/Reservation.cs
+++ LabProject/Models/Reservation.cs
@@ -3,7 +3,7 @@ public class Reservation
     public int Id { get; set; }
     public string ReserverName { get; set; }
     public int RoomId { get; set; } //Foreign key
-    public Room Room { get; set; }
+    public Room Room { get; set; } //nEREYE FOREIGN KEY OLDUĞUNUN CALSSINI VERDİK
     public DateTime ReservationDate { get; set; }
 
 
