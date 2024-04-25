d2fbad87b7102369701f38663abb0b734b124555
ALARAAKANTURK <c1812031@cankaya.edu.tr>
2024-04-25 11:48:23 +0300

    RoomId is came before the Room because it is important order for entity

--- LabProject/Models/Reservation.cs
+++ LabProject/Models/Reservation.cs
@@ -2,6 +2,7 @@ public class Reservation
 {       
     public int Id { get; set; }
     public string ReserverName { get; set; }
+    public int RoomId { get; set; }
     public Room Room { get; set; }
     public DateTime ReservationDate { get; set; }
 
