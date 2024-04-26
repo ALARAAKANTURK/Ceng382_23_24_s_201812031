1ed15b50bb043e5f4583c48d5e08922cb88c6d4c
ALARAAKANTURK <c1812031@cankaya.edu.tr>
2024-04-26 13:25:11 +0300

    UML.png is added to in UMLs folder

--- LabProject/obj/Debug/net8.0/LabProject.AssemblyInfo.cs
+++ LabProject/obj/Debug/net8.0/LabProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0683d2a65aad9e9a7e4adaf94c110d9ea0b05b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d69add3ffd8d5654782addbf86174ab6a7e422")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
